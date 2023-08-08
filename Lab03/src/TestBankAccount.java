ac9368303ebfbdf749e59161460245b08d7e77e5
dimitrisvla <benovla@yahoo.gr>
2023-08-08 15:11:22 +0300

    Update TestBankAccount.java

--- Lab03/src/TestBankAccount.java
+++ Lab03/src/TestBankAccount.java
@@ -1,4 +1,3 @@
-
 public class TestBankAccount {
 
 	public static void main(String[] args) {
@@ -7,6 +6,15 @@ public class TestBankAccount {
 		account.deposit(200);
 		account.withdraw(200);
 		account.printStatement();
+		account.withdraw(1500);
+		account.printStatement();
+		
+		BankAccount account2 = new BankAccount("Kostas", 0);
+		account2.deposit(100);
+		account2.deposit(200);
+		account2.withdraw(200);
+		account2.printStatement();
 	}
 
 }
+
